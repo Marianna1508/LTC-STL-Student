ea281ca2735c936719dfb2784e424fa68bdbc771
Marianna Vedernikova <mvedernikova@luxoft.com>
2020-12-16 12:45:33 +0200

    another version

--- ordered associative task/student/src/impl.cpp
+++ ordered associative task/student/src/impl.cpp
@@ -1,24 +1,15 @@
 #include "impl.h"
-#include <set>
-
-struct Range
-{
-    size_t lowRange;
-    size_t upRange;
-};
-
-std::map<Range, size_t> rangeMapping {
-        {Range{0, 29}, 1},
-        {Range{30, 49}, 2},
-        {Range{50, 69}, 3},
-        {Range{70, 89}, 4},
-        {Range{90, 100}, 5}
-};
 
 size_t convertMarkTo5PointSystem(size_t markIn100Point) {
-    std::set<size_t> rangesSet{0, 29, 30, 49, 50, 69, 70, 89, 90};
-    auto value = rangesSet.equal_range(markIn100Point);
-    auto findRange({value.first, value.second});
-    auto it = rangeMapping.find(findRange);
-    return it->second;
+    std::map <size_t, size_t> rangeMapping {
+        {29, 1}
+        , {49, 2}
+        , {69, 3}
+        , {89, 4}
+        , {90, 5}
+    };
+    auto result = rangeMapping.lower_bound(markIn100Point);
+    if (rangeMapping.end() != result) {
+        return result->second;
+    }
 }
